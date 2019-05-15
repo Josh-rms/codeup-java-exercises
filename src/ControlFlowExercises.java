43a6c0499dfe5fcf326c185fe07a2d747df88cb7
Josh Ramos <josh.r.938@gmail.com>
2019-05-15 10:45:19 -0500

    This is the do-while exercise section

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.println(i);
-            i++;
-        }
+//        int i = 0;
+//        do {
+//            if (i % 2 == 0) {
+//                System.out.println(i);
+//            }
+//            i++;
+//        } while (i <= 100);
+//
+//
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        } while (i >= -10);
+//
+//
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i < 1000000);
     }
 }
