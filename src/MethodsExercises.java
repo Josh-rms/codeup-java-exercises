6eb9adc3bd5203b9d95b15a4e724aa908a9b8d1a
Josh Ramos <josh.r.938@gmail.com>
2019-05-28 14:49:10 -0500

    This is the finished Methods exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,20 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+        System.out.println(Addition());
+        System.out.println(Subtraction());
+        System.out.println(Multiplication());
+        System.out.println(Division());
+    }
+    public static int Addition() {
+        return (1 + 1);
+    }
+    public static int Subtraction() {
+        return (2 - 1);
+    }
+    public static int Multiplication() {
+        return (2 * 2);
+    }
+    public static int Division() {
+        return (4 / 2);
+    }
+}
